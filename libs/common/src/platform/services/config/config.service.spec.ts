b2213fbb0ae97e0130920d9492586035725f7fa7
Thomas Rittson <trittson@bitwarden.com>
2023-06-13 11:21:17 +1000

    Add first server test

--- libs/common/src/platform/services/config/config.service.spec.ts
+++ libs/common/src/platform/services/config/config.service.spec.ts
@@ -1,5 +1,5 @@
 import { MockProxy, mock } from "jest-mock-extended";
-import { skip, take } from "rxjs";
+import { Subject, skip, take } from "rxjs";
 
 import { AuthService } from "../../../auth/abstractions/auth.service";
 import { ConfigApiServiceAbstraction } from "../../abstractions/config/config-api.service.abstraction";
@@ -9,6 +9,7 @@ import { StateService } from "../../abstractions/state.service";
 import { ServerConfigData } from "../../models/data/server-config.data";
 import {
   EnvironmentServerConfigResponse,
+  ServerConfigResponse,
   ThirdPartyServerConfigResponse,
 } from "../../models/response/server-config.response";
 
@@ -20,13 +21,20 @@ describe("ConfigService", () => {
   let authService: MockProxy<AuthService>;
   let environmentService: MockProxy<EnvironmentService>;
 
-  const storedConfigData = serverConfigDataFactory();
+  const storedConfigData = serverConfigDataFactory("storedConfig");
 
   beforeEach(() => {
     stateService = mock();
     configApiService = mock();
     authService = mock();
     environmentService = mock();
+    environmentService.urls = new Subject();
+
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
   });
 
   // Observables will start emitting as soon as this is created, so only create it
@@ -47,7 +55,23 @@ describe("ConfigService", () => {
   });
 
   describe("Fetches config from server", () => {
-    it.todo("on initial load");
+    it("on initial load", (done) => {
+      stateService.getServerConfig.mockResolvedValueOnce(storedConfigData);
+      configApiService.get.mockResolvedValue(serverConfigResponseFactory("server1"));
+
+      const configService = configServiceFactory();
+
+      configService.serverConfig$.pipe(skip(2), take(1)).subscribe((config) => {
+        try {
+          expect(config.gitHash).toEqual("server1");
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+
+      jest.advanceTimersByTime(100);
+    });
     it.todo("every hour");
     it.todo("when environment URLs change");
     it.todo("when fetchServerConfig() is called");
@@ -55,10 +79,14 @@ describe("ConfigService", () => {
   it.todo("Saves config to storage each time it's updated");
 });
 
-function serverConfigDataFactory() {
-  const data = new ServerConfigData({
+function serverConfigDataFactory(gitHash: string) {
+  return new ServerConfigData(serverConfigResponseFactory(gitHash));
+}
+
+function serverConfigResponseFactory(gitHash: string) {
+  return new ServerConfigResponse({
     version: "myConfigVersion",
-    gitHash: "myGitHash",
+    gitHash: gitHash,
     server: new ThirdPartyServerConfigResponse({
       name: "myThirdPartyServer",
       url: "www.example.com",
@@ -72,6 +100,4 @@ function serverConfigDataFactory() {
       feat3: "off",
     },
   });
-
-  return data;
 }
