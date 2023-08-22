c43c47cff659c728a52a529f9637613917967e87
Robyn MacCallum <robyntmaccallum@gmail.com>
2023-08-22 14:46:56 -0400

    Add check to hasMasterPasswordAndMasterKeyHash to make sure getMasterKey() has a value

--- libs/common/src/auth/services/user-verification/user-verification.service.ts
+++ libs/common/src/auth/services/user-verification/user-verification.service.ts
@@ -117,7 +117,8 @@ export class UserVerificationService implements UserVerificationServiceAbstracti
   async hasMasterPasswordAndMasterKeyHash(userId?: string): Promise<boolean> {
     return (
       (await this.hasMasterPassword(userId)) &&
-      (await this.cryptoService.getMasterKeyHash()) != null
+      (await this.cryptoService.getMasterKeyHash()) != null &&
+      (await this.stateService.getMasterKey()) != null // if a user unlocks with biometrics, no master key is stored
     );
   }
 
